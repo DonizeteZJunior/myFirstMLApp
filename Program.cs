f28d738ec568371d59587e8293a5ea4bd534edc8
DonizeteZJunior <91087078+DonizeteZJunior@users.noreply.github.com>
2023-06-15 16:12:34 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using MyMLApp;
+
+while (true)
+{
+    Console.WriteLine("Digite sua frase, para identificarmos o humor dela.");
+    var mensagem = Console.ReadLine();
+
+    var sampleData = new SentimentModel.ModelInput()
+    {
+        Col0 = mensagem
+    };
+
+    var result = SentimentModel.Predict(sampleData);
+
+    var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+}
