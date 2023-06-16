c2cffaf3d1ea0148edd3ffa022dc672d1a294b3b
DonizeteZJunior <91087078+DonizeteZJunior@users.noreply.github.com>
2023-06-16 10:50:55 -0300

    Adição de mais duas bases para teste, a da Amazon e do IMDB

--- Program.cs
+++ Program.cs
@@ -2,16 +2,37 @@
 
 while (true)
 {
-    Console.WriteLine("Digite sua frase, para identificarmos o humor dela.");
+    Console.WriteLine("Digite sua frase, para identificarmos o humor dela: ");
     var mensagem = Console.ReadLine();
 
+    /* Amazon ============ */
+    var amazonData = new AmazonModel.ModelInput()
+    {
+        Col0 = mensagem
+    };
+
+    var resultAmazon = AmazonModel.Predict(amazonData);
+
+    var sentimentAmazon = resultAmazon.PredictedLabel == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Amazon Sentiment: {sentimentAmazon} \n");
+
+    /* IMDB ============== */
+    var imdbData = new ImdbModel.ModelInput()
+    {
+        Col0 = mensagem
+    };
+
+    var sentimentIMDB = ImdbModel.Predict(imdbData);
+    Console.WriteLine($"Amazon Sentiment: {sentimentIMDB} \n");
+
+    /* Yelp ============== */
     var sampleData = new SentimentModel.ModelInput()
     {
         Col0 = mensagem
     };
 
-    var result = SentimentModel.Predict(sampleData);
+    var resultSentiment = SentimentModel.Predict(sampleData);
 
-    var sentiment = result.PredictedLabel == 1 ? "Positive" : "Negative";
-    Console.WriteLine($"Text: {sampleData.Col0}\nSentiment: {sentiment}");
+    var sentiment = resultSentiment.PredictedLabel == 1 ? "Positive" : "Negative";
+    Console.WriteLine($"Yelp Sentiment: {sentiment} \n");
 }
